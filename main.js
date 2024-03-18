6a103ee3eb6c8cedec2a32b50452f5ae8684ffee
oxBinaryBrain <udayg6269@gmail.com>
2024-03-18 12:12:39 +0530

    list of connections added

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ const net = require('net');
 const ADMIN_USERNAME = "admin";
 const ADMIN_PASSWORD = "adminpass";
 
-/
+// List of connected users
+const connectedUsers = {};
+
+// Function to broadcast message to all clients
+function broadcast(message) {
+    for (const userSocket of Object.values(connectedUsers)) {
+        userSocket.write(message);
+    }
+}
+
